21ee251f9d7e4def672e631ff2520c944c183850
chinheki <chinheki@gmail.com>
2023-12-13 01:59:26 +0900

    use createBrowserRouter instead

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import WeeklyTrain from "./pages/WeeklyTrain";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider,createBrowserRouter } from "react-router-dom";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
