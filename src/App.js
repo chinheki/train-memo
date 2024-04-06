f8efa831a52bdca85084ea27e1892ac5d573333e
chinheki <chinheki@gmail.com>
2024-04-06 14:56:43 +0900

    change upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{createContext,useEffect,useState} from "react";
+import React, { createContext, useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar";
 import "./App.scss";
 import "./i18n";
@@ -8,38 +8,48 @@ import diagram from "../public/assets/images/bk.jpg";
 import useLocalStorage from "./use-local-storage";
 import useTrainPlan from "./use-train-plan";
 export const DataContext = createContext(null);
-export const UpdateDataContext = createContext({update:()=>{},insert:()=>{},remove:()=>{}});
-export const TrainContext = createContext({updateTrainList:()=>{},trainList:[]});
+export const UpdateDataContext = createContext({
+  update: () => {},
+  insert: () => {},
+  remove: () => {}
+});
+export const TrainContext = createContext({
+  updateTrainList: () => {},
+  trainList: []
+});
 const App = () => {
   const value = useLocalStorage();
   const trainData = useTrainPlan();
   const onChangeToken = (v) => {
     value.updateToken(v.target.value);
-  
-}
+  };
   return (
     <div className="content">
-      <div className='bk' style={{ backgroundImage: `url(${diagram})` }} />
-    {
-      value.token ?
-        <DataContext.Provider value={{...value.data,token:value.token}}>
-          <UpdateDataContext.Provider value={{ update: value.updateData, insert: value.insertData, remove: value.deleteData }}>
+      <div className="bk" style={{ backgroundImage: `url(${diagram})` }} />
+      {value.token ? (
+        <DataContext.Provider value={{ ...value.data, token: value.token }}>
+          <UpdateDataContext.Provider
+            value={{
+              update: value.updateData,
+              insert: value.insertData,
+              remove: value.deleteData
+            }}
+          >
             <TrainContext.Provider value={trainData}>
-      
-                <Outlet />
-       
               <Sidebar />
+              <Outlet />
             </TrainContext.Provider>
           </UpdateDataContext.Provider>
         </DataContext.Provider>
-          : <div className="train-board">
-      <div className="train-row">
+      ) : (
+        <div className="train-board">
+          <div className="train-row">
             TOKEN:
-        <Input onChange={onChangeToken} value={value.token}></Input>
+            <Input onChange={onChangeToken} value={value.token}></Input>
+          </div>
         </div>
-        </div>
-      }
-      </div>
+      )}
+    </div>
   );
 };
 
--- src/pages/TrainBoard/UploadImages.js
+++ src/pages/TrainBoard/UploadImages.js
@@ -2,25 +2,40 @@ import React, { useState } from "react";
 import { PlusOutlined, DeleteTwoTone } from "@ant-design/icons";
 import { Button, Image, Upload } from "antd";
 
-
+function isUploadSupported() {
+    if (navigator.userAgent.match(/(Android (1.0|1.1|1.5|1.6|2.0|2.1))|(Windows Phone (OS 7|8.0))|(XBLWP)|(ZuneWP)|(w(eb)?OSBrowser)|(webOS)|(Kindle\/(1.0|2.0|2.5|3.0))/)) {
+        return false;
+    }
+    var elem = document.createElement('input');
+    elem.type = 'file';
+    return !elem.disabled;
+};
 
 const UploadImages = ({fileList, setFileList}) => {
-  const onUpload = async () => {
-    const handle = await window.showOpenFilePicker({
-      types: [
-        {
-          description: "Images",
-          accept: {
-            "image/*": [".png", ".gif", ".jpeg", ".jpg"]
-          }
-        }
-      ],
-      excludeAcceptAllOption: true,
-      multiple: true
-    });
-    const file = await handle[0].getFile();
+  const onUpload =async  (e) => {
+    const file=e.target.files[0]
+    // return;
+    // // if (isUploadSupported()) {
+    //   const handle = await window.showOpenFilePicker({
+    //     types: [
+    //     {
+    //       description: "Images",
+    //       accept: {
+    //         "image/*": [".png", ".gif", ".jpeg", ".jpg"]
+    //       }
+    //     }
+    //   ],
+    //   excludeAcceptAllOption: true,
+    //   multiple: true
+    //   }).catch((e) => { console.log(e) });
+    //   if (!handle) return;
+    //   const file = await handle[0].getFile();
+    if (!file) return;
     const src = URL.createObjectURL(file);
     setFileList((prev) => [...prev, { src, file }]);
+  //   } else {
+      
+  // }
   };
   const deleteImage = (image) => {
     const updatedPreviewImage = fileList.filter((img) => 
@@ -35,17 +50,6 @@ const UploadImages = ({fileList, setFileList}) => {
     setFileList(updatedPreviewImage);
   };
 
-  const uploadButton = (
-    <button
-      style={{
-        border: 0,
-        background: "none"
-      }}
-      type="button"
-    >
-      <PlusOutlined />
-    </button>
-  );
   return (
     <>
       {fileList.map((image, i) => (
@@ -54,22 +58,10 @@ const UploadImages = ({fileList, setFileList}) => {
           <img id={"frame-" + i} src={image.src} width="100px" height="100px" />
         </div>
       ))}
-      <Button onClick={onUpload}>
-        {fileList.length >= 8 ? null : uploadButton}
-      </Button>
-      {/* {previewImage && (
-        <Image
-          wrapperStyle={{
-            display: 'none',
-          }}
-          preview={{
-            visible: previewOpen,
-            onVisibleChange: (visible) => setPreviewOpen(visible),
-            afterOpenChange: (visible) => !visible && setPreviewImage(''),
-          }}
-          src={previewImage}
-        />
-      )} */}
+      {/* <Button onClick={onUpload}>
+      <PlusOutlined />
+      </Button> */}
+      <input id="myFileInput" type="file" accept="image/*" onChange={onUpload}></input>
     </>
   );
 };
